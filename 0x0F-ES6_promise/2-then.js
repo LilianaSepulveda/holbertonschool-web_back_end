2bf5b403e4cadb0422c178f851383acec603a33e
lilisep <lilianasepulveda13@gmail.com>
2023-03-22 11:10:41 -0500

    add task

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,8 @@
+export default function handleResponseFromAPI(promise) {
+    const result = promise
+      .then(() => ({ status: 200, body: 'success' }))
+      .catch(() => new Error(''));
+    console.log('Got a response from the API');
+    return result;
+  }
+  
